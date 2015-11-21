de747843b10ecac0cb6369247cc6218c7139090b
Dmitry Kuznetzov <megalord2@mail.ru>
2015-11-21 20:27:48 +0300

    add test for parser

--- CommandLine/Parser.java
+++ CommandLine/Parser.java
@@ -1,8 +1,8 @@
 import org.apache.commons.cli.*;
 
 class Parser {
-    private int leftOperand;
-    private int rightOperand;
+    private int left;
+    private int right;
     private String operation;
 
     public Parser(String[] args) throws ParseException {
@@ -19,15 +19,29 @@ class Parser {
 
         clOptions.addOption(rightOperand);
 
-        Option operation = new Option("o", "operation", true, "operation");
-        operation.setArgs(1);
-        operation.setArgName("operation ");
+        Option inputOperation = new Option("o", "operation", true, "operation");
+        inputOperation.setArgs(1);
+        inputOperation.setArgName("operation ");
 
-        clOptions.addOption(operation);
+        clOptions.addOption(inputOperation);
 
         CommandLineParser cmdLinePosixParser = new DefaultParser();
         org.apache.commons.cli.CommandLine commandLine = cmdLinePosixParser.parse(clOptions, args);
 
-        
+        left = Integer.decode( commandLine.getOptionValue("l"));
+        right = Integer.decode( commandLine.getOptionValue("r"));
+        operation = commandLine.getOptionValue("o");
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public String getOperation() {
+        return operation;
     }
 }
---
+++ test/ParserTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dimandus on 21.11.2015.
+ */
+public class ParserTest {
+
+    @Test
+    public void testParser() throws Exception {
+
+        String input = "-l 2 -r 3 -o +";
+
+        Parser parser = new Parser(input.split(" "));
+
+        assertEquals(2,parser.getLeft());
+        assertEquals(3,parser.getRight());
+        assertEquals("+",parser.getOperation());
+    }
+}
